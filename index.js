31cc80edc772b734fe60ce10c0e0433052ca08d5
RodrigoMinuzzo <rodrigominuzzo25@gmail.com>
2023-09-18 16:36:39 -0300

    incluído j.s

---
+++ index.js
@@ -0,0 +1,9 @@
+ let resposta = document.getElementById("resposta");
+ 
+
+ function inserir(valor) {
+    let resposta = document.getElementById("resposta");
+    resposta.textContent += valor;
+ }
+
+ 
